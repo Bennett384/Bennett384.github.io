ef86a4f4e51d6b3dea12ff84225477a55c874e91
James Bennett <jamesbennett@Jamess-MacBook-Air.local>
2019-10-21 09:03:09 -0500

    added some background and styling

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Bennett384.github.io
 Repo for Projects
+
+
+            My First App
+Use a Brewery API to let users search for breweries
+Users can search by city, state, or type
+The result of the search will show up on the screen underneath the user input box
+User can select favorite breweries and add them to a favorites list
--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Hi");
+// console.log("Hi");
+$(() =>{
+    $('form').on('submit', (event) =>{
+
+        event.preventDefault();
+
+        const userInput = $('input[type="text"]').val();
+    })
+
+
+
+
+
+console.log($);
+
+$.ajax({
+            url:"https://api.openbrewerydb.org/breweries"
+        }).then(
+            (data) => {
+                console.log(data);
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+
+        $('form').trigger('reset');
+
+    });
