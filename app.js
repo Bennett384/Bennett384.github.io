e2f81c4c093fc9c5fee4e1dc1dfba5ba8602def8
James Bennett <jamesbennett@Jamess-MacBook-Air.local>
2019-10-22 13:28:47 -0500

    commiting user input now selects character from list and displays some attributs

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 console.log($);
 
-$(() =>{
+$(()=>{
 
-    var settings = {
-    	"async": true,
-    	"crossDomain": true,
-    	"url": "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=NY&by_name=cooper&by_tag=patio&by_type=micro",
-    	"method": "GET",
-    	"headers": {
-    		"x-rapidapi-host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
-    		"x-rapidapi-key": "db29912736msh2b64448a0652826p1b58e5jsnda92dd86febf"
-    	}
-    }
+	$('form').on('submit', (event) =>{
+		event.preventDefault();
 
-    $.ajax(settings).done(function (response) {
-    	console.log(response);
-    })
+		const userInput = $('input[type="text"]').val();
 
+ 
 
+     $.ajax({
 
+        url:'https://rickandmortyapi.com/api/character/' + userInput,
 
+    }).then(
 
+        (data) => {
 
+    			// console.log(data);
 
+    		$('#name').html(data.name);
+    		$('#status').html(data.status);
+    		$('#species').html(data.species);
+    		$('#episodes').html(data.episode);           
+        },
+        (error) => {
+            console.log('bad');
+        }
+    );
 
-
-
+   });
 })
