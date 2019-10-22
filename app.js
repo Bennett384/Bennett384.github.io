27fc46bc2d6156c93f35c086dcfc0389dd1eb18c
James Bennett <jamesbennett@Jamess-MacBook-Air.local>
2019-10-21 20:56:43 -0500

    commiting changes to api and adding background image

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-// console.log("Hi");
+console.log($);
+
 $(() =>{
-    $('form').on('submit', (event) =>{
 
-        event.preventDefault();
+    var settings = {
+    	"async": true,
+    	"crossDomain": true,
+    	"url": "https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=NY&by_name=cooper&by_tag=patio&by_type=micro",
+    	"method": "GET",
+    	"headers": {
+    		"x-rapidapi-host": "brianiswu-open-brewery-db-v1.p.rapidapi.com",
+    		"x-rapidapi-key": "db29912736msh2b64448a0652826p1b58e5jsnda92dd86febf"
+    	}
+    }
 
-        const userInput = $('input[type="text"]').val();
+    $.ajax(settings).done(function (response) {
+    	console.log(response);
     })
 
 
 
 
 
-console.log($);
 
-$.ajax({
-            url:"https://api.openbrewerydb.org/breweries"
-        }).then(
-            (data) => {
-                console.log(data);
-            },
-            (error) => {
-                console.log(error);
-            }
-        )
 
-        $('form').trigger('reset');
 
-    });
+
+
+})
